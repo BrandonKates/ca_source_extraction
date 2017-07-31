47308063f9830b7a161d346fa19191eee319596e
Brandon Kates <bjk224@cornell.edu>
2017-07-31 10:54:59 -0400

    Added changes to saving and slight modifications.

--- use_cases/Hollis Lab Scripts/run_pipeline_HollisLab.m
+++ use_cases/Hollis Lab Scripts/run_pipeline_HollisLab.m
@@ -2,20 +2,27 @@
 clear;
 addpath(genpath('../NoRMCorre'));               % add the NoRMCorre motion correction package to MATLAB path
 gcp;        % start a parallel engine
-foldername = '';   
+ 
+manual = false;
+if manual
+    %foldername = 'F:\Test_run_pipeline_717';
+    foldername = 'C:\Users\User\Desktop\Test720';
+else
+    foldername = uigetdir;
+end
         % folder where all the files are located. Currently supported .tif,
         % .hdf5, .raw, .avi, and .mat files
 files = subdir(fullfile(foldername,'*.tif'));   % list of filenames (will search all subdirectories)
 FOV = [512,512];
 numFiles = length(files);
-
+ 
 %% motion correct (and save registered h5 files as 2d matrices (to be used in the end)..)
 % register files one by one. use template obtained from file n to
 % initialize template of file n + 1; 
-
+ 
 motion_correct = true;      % perform motion correction
 non_rigid = true;           % flag for non-rigid motion correction
-
+ 
 template = [];
 for i = 1:numFiles
     fullname = files(i).name;
@@ -37,9 +44,9 @@ for i = 1:numFiles
         convert_file(fullname,'h5',fullfile(folder_name,[file_name,'_mc.h5']));
     end
 end
-
+ 
 %% downsample h5 files and save into a single memory mapped matlab file
-
+ 
 if motion_correct
     if non_rigid
         h5_files = subdir(fullfile(foldername,'*_nr.h5'));  % list of h5 files (modify 
@@ -51,7 +58,7 @@ else
 end
     
 fr = 30;                                         % frame rate
-tsub = 10;                                        % degree of downsampling (for 30Hz imaging rate you can try also larger, e.g. 8-10)
+tsub = 5;                                        % degree of downsampling (for 30Hz imaging rate you can try also larger, e.g. 8-10)
 ds_filename = [foldername,'/ds_data.mat'];
 data_type = class(read_file(h5_files(1).name,1,1));
 data = matfile(ds_filename,'Writable',true);
@@ -92,18 +99,18 @@ for i = 1:numFiles
 end
 data.F_dark = F_dark;
 %% now run CNMF on patches on the downsampled file, set parameters first
-
+ 
 sizY = data.sizY;                       % size of data matrix
 patch_size = [32,32];                   % size of each patch along each dimension (optional, default: [32,32])
 overlap = [4,4];                        % amount of overlap in each dimension (optional, default: [4,4])
-
+ 
 patches = construct_patches(sizY(1:end-1),patch_size,overlap);
 K = 4;                                            % number of components to be found
 tau = 6;                                          % std of gaussian kernel (size of neuron) 
-p = 0;                                            % order of autoregressive system (p = 0 no dynamics, p=1 just decay, p = 2, both rise and decay)
+p = 0;                                            % oarder of autoregressive system (p = 0 no dynamics, p=1 just decay, p = 2, both rise and decay)
 merge_thr = 0.7;                                  % merging threshold
 sizY = data.sizY;
-
+ 
 options = CNMFSetParms(...
     'd1',sizY(1),'d2',sizY(2),...
     'search_method','ellipse','dist',3,...      % search locations when updating spatial components
@@ -120,17 +127,17 @@ options = CNMFSetParms(...
     'df_prctile',50,...                         % take the median of background fluorescence to compute baseline fluorescence 
     'fr',fr/tsub...
     );
-
+ 
 %% Run on patches (around 15 minutes)
-
+ 
 [A,b,C,f,S,P,RESULTS,YrA] = run_CNMF_patches(data,K,patches,tau,p,options);
-
+ 
 %% compute correlation image on a small sample of the data (optional - for visualization purposes) 
 Cn = correlation_image_max(single(data.Y),8);
-
+ 
 %% classify components
 [ROIvars.rval_space,ROIvars.rval_time,ROIvars.max_pr,ROIvars.sizeA,keep] = classify_components(data,A,C,b,f,YrA,options);
-
+ 
 %% run GUI for modifying component selection (optional, close twice to save values)
 run_GUI = false;
 if run_GUI
@@ -139,30 +146,32 @@ if run_GUI
     options = GUIout{2};
     keep = GUIout{3};    
 end
-
+ 
 %% view contour plots of selected and rejected components (optional)
+if run_GUI
 throw = ~keep;
 figure;
     ax1 = subplot(121); plot_contours(A(:,keep),Cn,options,0,[],Coor,1,find(keep)); title('Selected components','fontweight','bold','fontsize',14);
     ax2 = subplot(122); plot_contours(A(:,throw),Cn,options,0,[],Coor,1,find(throw));title('Rejected components','fontweight','bold','fontsize',14);
     linkaxes([ax1,ax2],'xy')
-    
+end
     %% keep only the active components    
 A_keep = A(:,keep);
 C_keep = C(keep,:);
-
+ 
+ 
 %% deconvolve (downsampled) temporal components plot GUI with components (optional)
-
-%tic;
-%[C_keep,f_keep,Pk,Sk,YrAk] = update_temporal_components_fast(data,A_keep,b,C_keep,f,P,options);
-%toc
-
-%plot_components_GUI(data,A_keep,C_keep,b,f,Cn,options)
-
+ 
+tic;
+[C_keep,f_keep,Pk,Sk,YrAk] = update_temporal_components_fast(data,A_keep,b,C_keep,f,P,options);
+toc
+ 
+plot_components_GUI(data,A_keep,C_keep,b,f,Cn,options)
+ 
 %% extract fluorescence and DF/F on native temporal resolution
 % C is deconvolved activity, C + YrA is non-deconvolved fluorescence 
 % F_df is the DF/F computed on the non-deconvolved fluorescence
-
+ 
 P.p = 2;                    % order of dynamics. Set P.p = 0 for no deconvolution at the moment
 C_us = cell(numFiles,1);    % cell array for thresholded fluorescence
 f_us = cell(numFiles,1);    % cell array for temporal background
@@ -176,11 +185,11 @@ for i = 1:numFiles
     [C_us{i},f_us{i},P_us{i},S_us{i},YrA_us{i}] = update_temporal_components_fast(h5_files(i).name,A_keep,b,Cin(1:end-1,:),Cin(end,:),P,options);
     b_us{i} = max(mm_fun(f_us{i},h5_files(i).name) - A_keep*(C_us{i}*f_us{i}'),0)/norm(f_us{i})^2;
 end
-
+ 
 prctfun = @(data) prctfilt(data,30,1000,300);       % first detrend fluorescence (remove 20%th percentile on a rolling 1000 timestep window)
 F_us = cellfun(@plus,C_us,YrA_us,'un',0);           % cell array for projected fluorescence
 Fd_us = cellfun(prctfun,F_us,'un',0);               % detrended fluorescence
-
+ 
 Ab_d = cell(numFiles,1);                            % now extract projected background fluorescence
 for i = 1:numFiles
     Ab_d{i} = prctfilt((bsxfun(@times, A_keep, 1./sum(A_keep.^2))'*b_us{i})*f_us{i},30,1000,300,0);
@@ -190,24 +199,41 @@ F0 = cellfun(@plus, cellfun(@(x,y) x-y,F_us,Fd_us,'un',0), Ab_d,'un',0);   % add
 F_df = cellfun(@(x,y) x./y, Fd_us, F0 ,'un',0);                            % DF/F value
 C_df = cellfun(@(x,y) x./y, C_us, F0 ,'un',0);                            % DF/F value
 %% detrend each segment and then deconvolve
-
-%% perform deconvolution
-Cd = cellfun(@(x) zeros(size(x)), Fd_us, 'un',0);
-Sp = cellfun(@(x) zeros(size(x)), Fd_us, 'un',0);
-bas = zeros(size(Cd{1},1),numFiles);
-c1 = bas;
-sn = bas;
-gn = cell(size(bas));
-options.p = 2;
-tt1 = tic;
-for i = 1:numFiles
- c_temp = zeros(size(Cd{i}));
- s_temp = c_temp;
- f_temp = Fd_us{i};
- parfor j = 1:size(Fd_us{i},1)
-     [c_temp(j,:),bas(j,i),c1(j,i),gn{j,i},sn(j,i),s_temp(j,:)] = constrained_foopsi(f_temp(j,:),[],[],[],[],options);
+ 
+ 
+% %% perform deconvolution
+ Cd = cellfun(@(x) zeros(size(x)), Fd_us, 'un',0);
+ Sp = cellfun(@(x) zeros(size(x)), Fd_us, 'un',0);
+ bas = zeros(size(Cd{1},1),numFiles);
+ c1 = bas;
+ sn = bas;
+ gn = cell(size(bas));
+ options.p = 2;
+ tt1 = tic;
+ for i = 1:numFiles
+     c_temp = zeros(size(Cd{i}));
+     s_temp = c_temp;
+     f_temp = Fd_us{i};
+     parfor j = 1:size(Fd_us{i},1)
+         [c_temp(j,:),bas(j,i),c1(j,i),gn{j,i},sn(j,i),s_temp(j,:)] = constrained_foopsi(f_temp(j,:),[],[],[],[],options);
+     end
+     Cd{i} = c_temp;
+     Sp{i} = s_temp;
+     toc(tt1);
  end
- Cd{i} = c_temp;
- Sp{i} = s_temp;
- toc(tt1);
-end
+ 
+ 
+ 
+ %% Save important stuff
+figure;
+[Coor, json_file] = plot_contours(A_keep,Cn,options,1);
+saveas(gcf,strcat(folder_name,'/contourmap','.fig')); close;
+pause(10)
+ 
+ 
+save(fullfile(folder_name,'Cd'),'Cd','-v7.3');
+save(fullfile(folder_name,'Sp'),'Sp','-v7.3');
+save(fullfile(folder_name,'Fdf'),'F_df','-v7.3');
+ 
+file_save_loc = strcat(folder_name,'/centroids','.json');
+savejson('jmesh',json_file,file_save_loc);
